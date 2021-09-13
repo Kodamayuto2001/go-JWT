cf9a6461958cc63025d3f98a3b3b1b42c18e7c60
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-09-13 20:29:31 +0900

    JWTを追加

---
+++ go.mod
@@ -0,0 +1,9 @@
+module go-JWT
+
+go 1.13
+
+require (
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/form3tech-oss/jwt-go v3.2.5+incompatible
+	github.com/gorilla/mux v1.8.0
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"go-JWT/auth"
+)
+
+type post struct {
+	Title 	string `json:"title"`
+	Tag		string `json:"tag"`
+	URL		string `json:"url"`
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.Handle("/public",public)
+	r.Handle("/private", auth.JwtMiddleware.Handler(private))
+	r.Handle("/auth", auth.GetTokenHandler)
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal("ListenAndServe:",err)
+	}
+}
+
+var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "hoge",
+        Tag:   "hoge",
+        URL:   "https://hoge",
+	}
+	json.NewEncoder(w).Encode(post)
+})
+
+var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: 	"piyo",
+		Tag:	"piyo",
+		URL:	"https://piyo",
+	}
+	json.NewEncoder(w).Encode(post)
+})
